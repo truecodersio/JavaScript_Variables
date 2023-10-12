a0b41668e5b6b5431921ba1e82cca41b05181145
Kim Weast <weastkim3@gmail.com>
2023-10-12 00:06:43 -0500

    update file

--- app.js
+++ app.js
@@ -4,143 +4,43 @@
 
 
 //Excercise 1
-for(let i = 1; i <= 100 ;i++)
-if( i % 3 ==0 );
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
-  console.log("FIZZ")
-}
+const firstName = "Rosie";
+let lastName = "Cotton";
+var age = 15;
 
-//Exercise 2
-// If the number is divisable by 3
-if (i % 3 == 0) { 
-  console.log("FIZZ");
-}
-//If the number is divisable by 5
-if (i % 5 == 0) { 
-  console.log("Buzz");
-}
-//Otherwise
-if(i % 5 == 0 && i % 3 == 0) { 
-  for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    console.log("FIZZBUZZ");
-}
-while (x > 3) { 
-  console.log("hello");
-} 
- do { 
-  console.log("Hello");
- } while (x > 3); { 
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = "true";
 
- }
- //Intializtion
- let i =0;
- //Condition
- while(i <= 100) { 
-  //If i is odd
-  if (i % 2 == 1)
-  //Print i on the console
-  i++;
-  console.log(i);
+console.log(`${language} was first release in ${createdYear}.');
 
- }
-//Increment
-i++;
-}
-i = 0;
-do{ 
-  if (i % 2 == 1) { 
-    console.log(i, "do while");
- }
- i++;
-} while (i <= 100);
-//If the number is divisable by 3 and 5
-if (i % 5 == 0 && i % 3 ==0) { 
-  //then print FIZZBUZZ
-  
-}
-//if the number is divisable by 3
-if (i % 3 == 0) { 
-  console.log("FIZZ")
-}
-//If the number is divisable by 5
-if ( i % 5 ==0);
-console.log("BUZZ");
-//Repeating  the Fizzbuss with While/Do-While
-while(i <= 100) { 
-  //If i is odd
-  if (i % 3 == 0)
-  //Print i on the console
-  i++;
-  console.log(i);
-
- }
-//Increment
-i++;
-}while (i<= 100 );
-
-do{ 
-  if (i % 2 == 1) { 
-    console.log(i, "do while");
- }
- i++;
-} while (i <= 100);
-//Exercise 4 find value
-//create a random numebr between 0 and 500
-let value = Math.round(Math.random()  *( 500 - 100) );
-
-//create a random number between 100 and 500
-let n = (math.round() * (500- 100) + 100);
-//I need a variable to help me remember
-let FoundValue = false;
-
-
-for(let j = 0; j <= n; j++){ 
-  if( j == value) 
-    didFindValue = true; 
-    console.log("Found value!");
-    //end the loop before its natural or expected end
-  }
-  break;
-//true/false
-if (!didFindValue) { 
-console.log("Did not find value");
-
-}
-//Exercise 5
-
-
-//create a random between 1 and 10
-let fizzDivor = Math.round(Math.random() * (1-10) + 1)
-//creatw a random number between 1 and 10
+let product = "T-shirty"
+Let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let inStock = false;
+let selectSize = "M";
 
 
 
-
-
-
-
-for (let j = start; j <= end); j++ { 
-  //check if its divisible by both numbers
-  if(j % fizzDivisor == 0 && j % bussDividor == 0){ 
-    console.log("FIZZBUZZ");
-
-  }
-  //Otherwise, it was not a divisable by both
-  else{ 
-    //check it it es divisable by at least one of em
-
-  if (j % fizzDivisor == 0){ 
-    console.log("FIZZ");
-  }
-  if (j % buzzDivisor == 0){ 
-    console.log("BUZZ")
-  }
 
 
 }
-console.log("Divisors:", fizzDivor, buzzDivisor);
-console.log("start/end", start, end);
+
+Let title = "Name of the Wind";
+Let author = Patrick Rothfuss;
+Let pageCount = 722;
+let bookMark = 456;
+let hasRead = true;
+
+console.log ('${title} by ${author} is ${pageCount} pages long. Your last bookmark qas page ${bookmark}).
+
+);
+
+
+
+
+
+
 
 
